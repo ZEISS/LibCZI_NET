0d8bd64bceb635ee0985d75dcf67370ae05a73ac
Juergen Bohl <juergen.bohl@zeiss.com>
2025-05-02 11:10:21 +0200

    fix URLs

--- dotnet/LibCZI_Net.UnitTests/InputStreamsRepository.cs
+++ dotnet/LibCZI_Net.UnitTests/InputStreamsRepository.cs
@@ -18,7 +18,8 @@ namespace LibCZI_Net.UnitTests
             return Factory.CreateInputStream(
                 "curl_http_inputstream",
                 // "https://zenodo.org/records/14968770/files/2025_01_27__0007_offline_Zen_3_9_5.czi?download=1",
-                "https://ptahmose.de/nextcloud/s/ZaFwaZxKpaMrbZo/download/2025_01_27__0007_offline_Zen_3_9_5.czi",
+                //"https://ptahmose.de/nextcloud/s/ZaFwaZxKpaMrbZo/download/2025_01_27__0007_offline_Zen_3_9_5.czi",
+                "https://ptahmose.de/nextcloud/public.php/dav/files/ZaFwaZxKpaMrbZo",
                 new Dictionary<string, object>() { { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" }, });
 
         }
@@ -27,7 +28,8 @@ namespace LibCZI_Net.UnitTests
         {
             return Factory.CreateInputStream(
                 "curl_http_inputstream",
-                "https://ptahmose.de/nextcloud/s/NyLBdoKjxDaw4m9/download/DCV_30MB_gamma_spline.czi",
+                //"https://ptahmose.de/nextcloud/s/NyLBdoKjxDaw4m9/download/DCV_30MB_gamma_spline.czi",
+                "https://ptahmose.de/nextcloud/public.php/dav/files/NyLBdoKjxDaw4m9",
                 new Dictionary<string, object>() { { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" }, });
         }
     }
