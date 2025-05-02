df52c888b3296de1f013d798dc2e0e0a8dcee6e1
Juergen Bohl <juergen.bohl@zeiss.com>
2025-05-02 22:25:37 +0200

    Update InputStreamsRepository with new stream options
    Added `CurlHttpFollowLocation` option to input stream methods.
    Removed unnecessary `using` directives and updated commented URLs for consistency.

--- dotnet/LibCZI_Net.UnitTests/InputStreamsRepository.cs
+++ dotnet/LibCZI_Net.UnitTests/InputStreamsRepository.cs
@@ -5,11 +5,7 @@
 namespace LibCZI_Net.UnitTests
 {
     using LibCZI_Net.Interface;
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class InputStreamsRepository
     {
@@ -18,19 +14,26 @@ namespace LibCZI_Net.UnitTests
             return Factory.CreateInputStream(
                 "curl_http_inputstream",
                 // "https://zenodo.org/records/14968770/files/2025_01_27__0007_offline_Zen_3_9_5.czi?download=1",
-                //"https://ptahmose.de/nextcloud/s/ZaFwaZxKpaMrbZo/download/2025_01_27__0007_offline_Zen_3_9_5.czi",
+                // "https://ptahmose.de/nextcloud/s/ZaFwaZxKpaMrbZo/download/2025_01_27__0007_offline_Zen_3_9_5.czi",
                 "https://ptahmose.de/nextcloud/public.php/dav/files/ZaFwaZxKpaMrbZo",
-                new Dictionary<string, object>() { { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" }, });
-
+                new Dictionary<string, object>()
+                {
+                    { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" },
+                    { StreamClassPropertyKeys.CurlHttpFollowLocation, true },
+                });
         }
 
         public static IInputStream CreateStreamFor3chWithLutDocument()
         {
             return Factory.CreateInputStream(
                 "curl_http_inputstream",
-                //"https://ptahmose.de/nextcloud/s/NyLBdoKjxDaw4m9/download/DCV_30MB_gamma_spline.czi",
+                // "https://ptahmose.de/nextcloud/s/NyLBdoKjxDaw4m9/download/DCV_30MB_gamma_spline.czi",
                 "https://ptahmose.de/nextcloud/public.php/dav/files/NyLBdoKjxDaw4m9",
-                new Dictionary<string, object>() { { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" }, });
+                new Dictionary<string, object>()
+                {
+                    { StreamClassPropertyKeys.CurlHttpUserAgent, "libCZI" },
+                    { StreamClassPropertyKeys.CurlHttpFollowLocation, true },
+                });
         }
     }
 }
